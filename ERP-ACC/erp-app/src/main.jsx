977477de32630151d9a1f7769d4a4ee1e0445139
manfredgunardi-star <manfredgunardi-star@users.noreply.github.com>
2026-04-15 11:21:40 +0700

    feat(ui): install antd and setup ConfigProvider with id_ID locale

--- ERP-ACC/erp-app/src/main.jsx
+++ ERP-ACC/erp-app/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ConfigProvider, App as AntdApp } from 'antd'
+import idID from 'antd/locale/id_ID'
+import 'dayjs/locale/id'
+import dayjs from 'dayjs'
 import './index.css'
 import App from './App.jsx'
 
+dayjs.locale('id')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ConfigProvider locale={idID}>
+      <AntdApp>
+        <App />
+      </AntdApp>
+    </ConfigProvider>
   </StrictMode>,
 )
